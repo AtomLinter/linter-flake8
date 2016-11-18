3737fe508272c94f9341fef9a19db79eb2050941
Johan Walles <johan.walles@gmail.com>
2016-11-18 21:42:53 +0100

    Remedy review feedback

--- spec/linter-flake8-spec.js
+++ spec/linter-flake8-spec.js
@@ -12,19 +12,16 @@ describe('The flake8 provider for Linter', () => {
   const lint = require('../lib/main.js').provideLinter().lint;
 
   beforeEach(() => {
-    // This whole beforeEach function is inspired by:
-    // https://github.com/AtomLinter/linter-jscs/pull/295/files
-    //
-    // See also:
-    // https://discuss.atom.io/t/activationhooks-break-unit-tests/36028/8
+    // Info about this beforeEach() implementation:
+    // https://github.com/AtomLinter/Meta/issues/15
     const activationPromise =
       atom.packages.activatePackage('linter-flake8');
 
     waitsForPromise(() =>
-      atom.packages.activatePackage('language-python'));
+      atom.packages.activatePackage('language-python').then(() =>
+        waitsForPromise(() =>
+          atom.workspace.open(goodPath))));
 
-    waitsForPromise(() =>
-      atom.workspace.open(goodPath));
 
     atom.packages.triggerDeferredActivationHooks();
     waitsForPromise(() => activationPromise);
